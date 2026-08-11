36c54c089fb1520a78e3db5a603619eb789bbe24
Camposgrind <sergiocamposmartin@gmail.com>
2026-08-11 09:34:49 +0100

    Fine tune landing page with styles and logo

--- frontend/src/app/features/home/band-card/band-card.component.ts
+++ frontend/src/app/features/home/band-card/band-card.component.ts
@@ -1,9 +1,12 @@
 import { Component, input } from '@angular/core';
+import { NgClass } from '@angular/common';
 import { BandListItemModel } from '../../../infrastructure/api/models';
+import { CountryFlagPipe } from '../../../shared/pipes/country-flag.pipe';
 
 @Component({
   selector: 'dmc-band-card',
   standalone: true,
+  imports: [CountryFlagPipe, NgClass],
   templateUrl: './band-card.component.html',
   styleUrl: './band-card.component.scss',
 })
